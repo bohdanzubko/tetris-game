2db03186f181519f0bfae5e4cdd0fbae6741caa2
Bohdan Zubko <108411272+bohdanzubko@users.noreply.github.com>
2023-04-26 19:15:21 +0300

    Fill Blocks class methods
    Added methods for:
    -block rotating(clockwise/counterclockwise)
    -block moving
    -reset block

--- Blocks.cs
+++ Blocks.cs
@@ -1,4 +1,6 @@
-﻿namespace TetrisGame
+﻿using System.Collections.Generic;
+
+namespace TetrisGame
 {
    public abstract class Block
     {
@@ -11,27 +13,37 @@
 
         public Block()
         {
-
+            offset = new Position(StartOffset.Row, StartOffset.Column);
         }
 
         public void RotateCW()
         {
-
+            rotationState = (rotationState + 1) % Tiles.Length;
         }
 
         public void RotateCCW()
         {
-
+            if (rotationState == 0)
+            {
+                rotationState = Tiles.Length - 1;
+            }
+            else
+            {
+                rotationState--;
+            }
         }
 
         public void Move(int rows, int columns)
         {
-
+            offset.Row += rows;
+            offset.Column += columns;
         }
 
         public void Reset()
         {
-
+            rotationState = 0;
+            offset.Row = StartOffset.Row;
+            offset.Column = StartOffset.Column;
         }
     }
 
